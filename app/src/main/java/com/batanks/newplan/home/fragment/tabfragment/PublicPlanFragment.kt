ed18eb16f412cf7688e9ed4dee699c83be7fe189
Vinoth Sivaraj <vinoth.sivaraj@people10.com>
2020-04-12 20:17:22 +0530

    UI alteration

--- app/src/main/java/com/batanks/newplan/home/fragment/tabfragment/PublicPlanFragment.kt
+++ app/src/main/java/com/batanks/newplan/home/fragment/tabfragment/PublicPlanFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
 import com.batanks.newplan.R
+import kotlinx.android.synthetic.main.fragment_public_new_plan.*
 
 class PublicPlanFragment : Fragment() {
 
@@ -15,5 +18,13 @@ class PublicPlanFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_public_new_plan, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val items = listOf("Material", "Design", "Components", "Android")
+        val adapter = ArrayAdapter(requireContext(), R.layout.auto_complete_text_view, items)
+        (categoryTextField.editText as? AutoCompleteTextView)?.setAdapter(adapter)
     }
 }
